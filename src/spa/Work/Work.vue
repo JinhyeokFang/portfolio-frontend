0f399f52af1f197186f9b49d1d6a08f8269b58d5
Shin-JaeHeon <heoni0504@gmail.com>
2018-06-18 10:44:57 +0900

    package.json 의존성 변경 및 Work.vue에서 v-for로 랜더링 되도록 변경

--- src/spa/Work/Work.vue
+++ src/spa/Work/Work.vue
@@ -7,7 +7,7 @@
           <h1>More View</h1>
           <div class="bar"></div>
         </div>
-        <div class="order-select"> 
+        <div class="order-select">
           <select>
             <option value="">최신순</option>
             <option value="">최신순</option>
@@ -16,7 +16,7 @@
         </div>
       </div>
       <section class="cards">
-        <work-card :overview="overview" :groups="groups"></work-card>
+        <work-card v-for="(item,index) in list" :overview="item.overview" :groups="item.groups" :key="index"></work-card>
       </section>
     </section>
   </section>
@@ -26,18 +26,27 @@
 <script>
   import Card from '../../components/Card.vue';
   import Navbar from '../../components/Navbar.vue';
+
   export default {
     name: 'Work',
     data() {
       return {
-        overview: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate neque consequatur ab nulla.',
-        team: ['홍길동', '홍길동', '홍길동'],
-        groups: ['생활', '모바일'],
+        list: [{
+          overview: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate neque consequatur ab nulla.',
+          team: ['홍길동', '홍길동', '홍길동'],
+          groups: ['생활', '모바일'],
+        }, {
+          overview: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate neque consequatur ab nulla.',
+          team: ['홍길동', '홍길동', '홍길동'],
+          groups: ['생활', '모바일'],
+        }],
       };
     },
     components: {
       'work-card': Card,
       'navigation-bar': Navbar,
+    },
+    created() {
     },
     methods: {},
   };
@@ -47,29 +56,34 @@
 <style scoped>
   .wrapper {
     max-width: 80%;
-    margin:  0 auto;
+    margin: 0 auto;
   }
-  .header{
+
+  .header {
     height: 100px;
     display: flex;
     flex-direction: row;
     align-items: center;
     margin-bottom: 50px;
   }
-  .header > .title > h1{
+
+  .header > .title > h1 {
     font-size: 2rem;
     text-align: left;
   }
-  .bar{
+
+  .bar {
     width: 190px;
     height: 10px;
     margin: 20px 0px 0px 0px;
     background-color: #000000;
   }
-  .order-select{
+
+  .order-select {
     margin-left: 10px;
   }
-  .order-select select{
+
+  .order-select select {
     outline: none;
     margin-left: 80px;
     -webkit-appearance: none;
@@ -77,7 +91,7 @@
     background: transparent;
     font-size: 1.2rem;
     text-align: center;
-    text-align-last: center;     /* chrome */
+    text-align-last: center; /* chrome */
     width: 140px;
     height: 40px;
     border: 3px solid black;
@@ -87,10 +101,13 @@
     background-position-x: 100%;
     background-position-y: 5px;
   }
-  /* IE compatibility 
-  .order-select select::-ms-expand { 
-    display: none; 
-    background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSdibGFjaycgaGVpZ2h0PScyNCcgdmlld0JveD0nMCAwIDI0IDI0JyB3aWR0aD0nMjQnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2Zyc+PHBhdGggZD0nTTcgMTBsNSA1IDUtNXonLz48cGF0aCBkPSdNMCAwaDI0djI0SDB6JyBmaWxsPSdub25lJy8+PC9zdmc+); 
+
+  /* IE compatibility
+  .order-select select::-ms-expand {
+    display: none;
+    background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSdibGFjaycgaGVpZ2h0PScyNCcgdmlld0JveD0nMCAwIDI
+    0IDI0JyB3aWR0aD0nMjQnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2Zyc
+    +PHBhdGggZD0nTTcgMTBsNSA1IDUtNXonLz48cGF0aCBkPSdNMCAwaDI0djI0SDB6JyBmaWxsPSdub25lJy8+PC9zdmc+);
   } */
 
   .cards {
