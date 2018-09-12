708e97e997d8220fb660b183e72241c892fa5dea
Shin-JaeHeon <heoni0504@gmail.com>
2018-09-12 13:47:07 +0900

    dev init

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
-Vue.use(require('vue-prevent-parent-scroll'))
+Vue.use(require('vue-prevent-parent-scroll'));
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
